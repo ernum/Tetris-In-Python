65ac302e236f85935ef2a5d94d5834a2a99ba412
unknown <umeh@kth.se>
2020-04-22 20:49:09 +0200

    Update figure.py

--- src/figure.py
+++ src/figure.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 
 
 class Figure(object):
@@ -7,32 +8,32 @@ class Figure(object):
         O, I, S, Z, L, J and T. Input that one of those as
         string args to generate them. """
 
-    O = [[0, 0, 0, 0],
-         [0, 0, 0, 0],
-         [0, 0, 0, 0],
-         [0, 0, 0, 0]]
-    I = [[0, 0, 0, 0],
-         [0, 0, 0, 0],
+    O = [[1, 1, 0, 0],
+         [1, 1, 0, 0],
          [0, 0, 0, 0],
          [0, 0, 0, 0]]
-    S = [[0, 0, 0, 0],
-         [0, 0, 0, 0],
-         [0, 0, 0, 0],
-         [0, 0, 0, 0]]
-    Z = [[0, 0, 0, 0],
-         [0, 0, 0, 0],
-         [0, 0, 0, 0],
-         [0, 0, 0, 0]]
-    L = [[0, 0, 0, 0],
-         [0, 0, 0, 0],
+    I = [[1, 0, 0, 0],
+         [1, 0, 0, 0],
+         [1, 0, 0, 0],
+         [1, 0, 0, 0]]
+    S = [[0, 1, 1, 0],
+         [1, 1, 0, 0],
          [0, 0, 0, 0],
          [0, 0, 0, 0]]
-    J = [[0, 0, 0, 0],
-         [0, 0, 0, 0],
+    Z = [[1, 1, 0, 0],
+         [0, 1, 1, 0],
          [0, 0, 0, 0],
          [0, 0, 0, 0]]
-    T = [[0, 0, 0, 0],
-         [0, 0, 0, 0],
+    L = [[1, 0, 0, 0],
+         [1, 0, 0, 0],
+         [1, 1, 0, 0],
+         [0, 0, 0, 0]]
+    J = [[0, 1, 0, 0],
+         [0, 1, 0, 0],
+         [1, 1, 0, 0],
+         [0, 0, 0, 0]]
+    T = [[1, 1, 1, 0],
+         [0, 1, 0, 0],
          [0, 0, 0, 0],
          [0, 0, 0, 0]]
 
@@ -40,6 +41,17 @@ class Figure(object):
         pygame.init()
         self.colour = colour
         self.shape = shape
+
+    def shape_from_input(self):
+        return {
+            'O': self.O,
+            'I': self.I,
+            'S': self.S,
+            'Z': self.Z,
+            'L': self.L,
+            'J': self.J,
+            'T': self.T,
+        }.get(self.shape, None)
 
     def drawFigure(self):
         # Följande kan vi flytta till gameboard när vi börjar med den.
@@ -50,12 +62,17 @@ class Figure(object):
                     pygame.quit()
             window.fill((0, 0, 0))
 
-            for i in range(len(self.O)):
-                for j in range(len(self.O[i])):
-                    if self.O[j] == 0:
-                        pygame.draw.rect(window, self.colour,
-                                         pygame.Rect(20, 20, 20, 20))
-                    pygame.display.update()
+            shape = self.shape_from_input()
+            if shape:  # 0 is a falsy value so returns false if element is 0
+                for y, row in enumerate(shape):
+                    for x, element in enumerate(row):
+                        if element:
+                            pygame.draw.rect(window, self.colour,
+                                             pygame.draw.rect(window, self.colour, pygame.Rect(((20*x)+20, (20*y)+20), (20, 20))))
+                pygame.display.update()
+            else:
+                raise ValueError(
+                    'The shape {} does not exist.'.format(self.shape))
 
 
 figure = Figure((255, 255, 255), shape="O")
