9bf0fb0e01d37f46892a1ccae0c88ee1b1b53e25
jerikso <jerikso@kth.se>
2020-04-29 17:49:59 +0200

    Add sideways collision fix #59 (#66)
    * Add sideways collision fix #59
    
    * Add option for rotation

--- src/gameboarddemo.py
+++ src/gameboarddemo.py
@@ -44,14 +44,27 @@ def matrix_merge(currentMatrix, figure):
         for i in range(len(fig_m)):
             if fig_m[j][i] != 0:
                 row = j + figure.matrixPosY
-                column = i  + figure.matrixPosX
+                column = i + figure.matrixPosX
                 under = currentMatrix[row+1][column]
                 if under != 0:
                     collisionFound = True
                 newMatrix[row][column] = fig_m[j][i]
 
-
     return newMatrix, collisionFound
+
+def checkCollision(currentMatrix, figure, movement, rotation):
+    # Right now first pos of fig ([0]). Will need to get the right rotation as well.
+    fig_m = figure.shapeList[(figure.currentRotation + rotation) % 4]
+
+    for j in range(len(fig_m[0])):
+        for i in range(len(fig_m)):
+            if fig_m[j][i] != 0:
+                row = j + figure.matrixPosY + movement[1]
+                column = i  + figure.matrixPosX + movement[0]
+                nextBlock = currentMatrix[row][column]
+                if nextBlock != 0:
+                    return True
+    return False
 
 def nextShape(queue, currentMatrix):
     figure = queue.next()
@@ -93,9 +106,11 @@ while True:
             if event.key == pg.K_z:
                 f.rotate_anticlockwise()
             if event.key == pg.K_LEFT:
-                f.move_left()
+                if not checkCollision(gb.board,f,(-1,0),0):
+                    f.move_left()
             if event.key == pg.K_RIGHT:
-                f.move_right()
+                if not checkCollision(gb.board,f,(1,0),0):
+                    f.move_right()
             if event.key == pg.K_DOWN:
                 f.move_down()
 
