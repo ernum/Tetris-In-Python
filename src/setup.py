51c421c5ccd6926b16c9cbb5a4ab9121a2083604
umeh <umeh@kth.se>
2020-05-09 02:45:38 +0200

    Add setup file

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+import re
+from setuptools import setup, find_packages
+required = ["pygame"]
+
+setup(
+    name="Tetris-in-Python",
+    version="0.0.1",
+    description="This is a Tetris game built using python and pygame.",
+    url="https://gits-15.sys.kth.se/jerikso/tetris-in-python",
+    author="hostedt, jerikso and umeh",
+    packages=find_packages(where="src"),
+    required=required,
+)
