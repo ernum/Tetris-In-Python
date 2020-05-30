4b4acc94d4735e61f72055da572d6ddb94f949dd
Ernest Umeh <umeh@kth.se>
2020-05-30 22:39:56 +0200

    Fix setup.py not properly installing the game (#141)
    * Fix issues
    
    * add MANIFEST.in
    
    * Change soundtrack to .ogg

--- setup.py
+++ setup.py
@@ -7,7 +7,14 @@ setup(
     description="This is a Tetris game built using python and pygame.",
     url="https://gits-15.sys.kth.se/jerikso/tetris-in-python",
     author="hostedt, jerikso and umeh",
-    packages=find_packages(where="src"),
+    packages=find_packages(where="."),
+    package_data={
+        "Tetris.Sound.SoundEffect": ["*.wav"],
+        "Tetris.Sound.Soundtrack": ["*.ogg"],
+        "Tetris.images": ["*.png"],
+        "Tetris.fonts": ["*.ttf"],
+    },
+    include_package_data=True,
     install_requires=[
         'pygame'
     ],
